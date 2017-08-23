ad7a7d32f9070eac02c2bcd8501b8324c1585583
srikanth-venturew <srikanth@venturew.com>
2017-08-23 18:32:42 +0530

    added new views

---
+++ app_server/controllers/locations.js
@@ -0,0 +1,14 @@
+
+
+
+module.exports.homeList = function(req,res){
+    res.render('locations-list',{title:'Home'});
+}
+
+module.exports.locationInfo = function(req,res){
+    res.render('location-info',{title:'Location Info'});
+}
+
+module.exports.addReview = function(req,res){
+    res.render('index',{title:'Add review'});
+}
--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -1,9 +1,20 @@
 var express = require('express');
 var router = express.Router();
 
-var ctrlMain = require('../controllers/main');
 
-/* GET home page. */
-router.get('/', ctrlMain.index);
+var ctrlLocations = require('../controllers/locations');
+var ctrlOthers = require('../controllers/others');
+
+
+/* Locations page. */
+router.get('/',ctrlLocations.homeList);
+router.get('/location',ctrlLocations.locationInfo);
+router.get('/location/review/new',ctrlLocations.addReview);
+
+
+/* about page. */
+router.get('/about',ctrlOthers.about);
+
+
 
 module.exports = router;
