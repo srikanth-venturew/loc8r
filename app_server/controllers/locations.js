893e7b1e0e91f38507c9c9c7f519dc3cd3f045af
srikanth-venturew <srikanth@venturew.com>
2017-08-24 15:44:38 +0530

    view info moved to controllers

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -1,6 +1,33 @@
 
 module.exports.homeList = function(req,res){
-    res.render('locations-list',{title:'Home'});
+    res.render('locations-list',{
+        title:'Loc8r - find a place to work with wifi',
+        pageHeader:{
+            title:'Loc8r',
+            strapLine:'Find places to work with wifi near you'
+        },
+        sidebar: "Looking for wifi and a seat? Loc8r helps you find places to work when out and about. Perhaps with coffee, cake or a pint? Let Loc8r help you find the place you're looking for.",
+        locations:[
+            {
+                name:'Starcups',
+                address:'125 High street, Reading , RG6 1PS',
+                rating:3,
+                facilities:['Hot drinks','Food','Free wifi']
+            },
+            {
+                name:'Cafe Hero',
+                address:'125 High street, Reading , RG6 2PS',
+                rating:4,
+                facilities:['Hot drinks','Food','Free wifi']
+            },
+            {
+                name:'Burger Queen',
+                address:'125 High street, Reading , RG6 1PS',
+                rating:3,
+                facilities:['Hot drinks','Food','Free wifi']
+            }
+        ]
+    });
 }
 
 module.exports.locationInfo = function(req,res){
